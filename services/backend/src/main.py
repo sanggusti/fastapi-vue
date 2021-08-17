32bacc6e511657c8b98160fc4a2c15b59ba5ccaa
sanggusti <gustiowinata0@gmail.com>
2021-08-17 11:05:25 +0700

    Add more features

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -5,10 +5,18 @@ from tortoise import Tortoise
 from src.database.register import register_tortoise
 from src.database.config import TORTOISE_ORM
 
+
+# enable schemas to read relationship between models
 Tortoise.init_models(["src.database.models"], "models")
 
-app = FastAPI()
+"""
+import 'from src.routes import users, notes' must be after 'Tortoise.init_models'
+why?
+https://stackoverflow.com/questions/65531387/tortoise-orm-for-python-no-returns-relations-of-entities-pyndantic-fastapi
+"""
+from src.routes import users, notes
 
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,10 +25,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(users.router)
+app.include_router(notes.router)
 
-register_tortoise(app, config=TORTOISE_ORM, generate_schemas=True)
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return "Hello, World!"
